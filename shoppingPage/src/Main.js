9edb12f5df0b607a4ef6f98af314adb196b466af
omur8819 <omur8819@gmail.com>
2020-11-14 00:32:17 +0300

    product_data.json edited

--- shoppingPage/src/Main.js
+++ shoppingPage/src/Main.js
@@ -4,28 +4,35 @@ import { SafeAreaView, View, Text, FlatList, StyleSheet, Alert, TextInput } from
 import proudctData from './product_data.json'
 import { ProductCard } from './components'
 const App = () => {
+
     const [searchValue, setSearchValue] = useState("");
+
     const [displayList, setDisplayList] = useState([])
+
     const renderListItem = ({ item }) => <ProductCard product={item} />
+
     useEffect(() => {
-        // Alert.alert("Clarushop", "Hoşgeldiniz, keyifli alışverişler..");
+        // Alert.alert("Trend Shop", "Welcome, have a nice shopping..");
         setDisplayList(proudctData)
     }, [])
+
     useEffect(() => {
         const filteredValue = proudctData.filter(item => {
             const text = searchValue.toUpperCase();
             const productTitle = item.title.toUpperCase();
             return productTitle.indexOf(text) > -1;
         })
-        setDisplayList(filteredValue)
+
+    setDisplayList(filteredValue)
     }, [searchValue])
+
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <View style={{ flex: 1 }}>
-                <Text style={styles.banner}>CLARUSHOP</Text>
+                <Text style={styles.banner}>TREND SHOP</Text>
                 <View style={styles.searchBar}>
                     <TextInput
-                        placeholder="Ürün ara..."
+                        placeholder="Search a product..."
                         onChangeText={(value) => setSearchValue(value)}
                         style={styles.searchText}
                     />
